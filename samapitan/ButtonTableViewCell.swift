18673665f132b7f74cc085fa844f185c6fa74001
Andrew Fang <andrewbrianfang@gmail.com>
2016-05-17 17:34:51 -0700

    Add buttons for cancel and resole

---
+++ samapitan/ButtonTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ButtonTableViewCell.swift
+//  samapitan
+//
+//  Created by Andrew Fang on 5/17/16.
+//  Copyright © 2016 Fang Industries. All rights reserved.
+//
+
+import UIKit
+
+class ButtonTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var actionButton: UIButton!
+
+}
--- samapitan/RequestSettingsViewController.swift
+++ samapitan/RequestSettingsViewController.swift
@@ -19,6 +19,7 @@ class RequestSettingsViewController: UIViewController, UITableViewDelegate, UITa
         case Desc = 1
         case Urgency = 2
         case People = 3
+        case Buttons = 4
     }
     
     override func viewDidLoad() {
@@ -29,19 +30,23 @@ class RequestSettingsViewController: UIViewController, UITableViewDelegate, UITa
         self.tableView.backgroundColor = UIColor.appBackgroundColor()
     }
     
-    let headerSections = ["TITLE", "DESCRIPTION", "URGENCY", "PEOPLE HELPING OUT"]
+    let headerSections = ["TITLE", "DESCRIPTION", "URGENCY", "PEOPLE HELPING OUT", "Buttons"]
     
     func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         
-        let view = UIView(frame: CGRectMake(0, 0, tableView.frame.size.width, 18))
-        
-        let label = UILabel(frame: CGRectMake(10, 5, tableView.frame.size.width, 18))
-        label.font = UIFont.systemFontOfSize(12, weight: UIFontWeightThin)
-        label.text = self.headerSections[section]
-        view.addSubview(label)
-        view.backgroundColor = UIColor.appBackgroundColor()
-        
-        return view
+        if (section < headerSections.count - 1) {
+            let view = UIView(frame: CGRectMake(0, 0, tableView.frame.size.width, 18))
+            
+            let label = UILabel(frame: CGRectMake(10, 5, tableView.frame.size.width, 18))
+            label.font = UIFont.systemFontOfSize(12, weight: UIFontWeightThin)
+            label.text = self.headerSections[section]
+            view.addSubview(label)
+            view.backgroundColor = UIColor.appBackgroundColor()
+            
+            return view
+        } else {
+            return UIView(frame: CGRectMake(0, 0, tableView.frame.size.width, 0))
+        }
     }
     
     func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
@@ -53,8 +58,9 @@ class RequestSettingsViewController: UIViewController, UITableViewDelegate, UITa
             label.text = "Choosing immediately will alert nearby volunteers"
             view.addSubview(label)
             return view
+        } else {
+            return UIView(frame: CGRectMake(0, 0, tableView.frame.size.width, 0))
         }
-        return nil
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
@@ -72,6 +78,8 @@ class RequestSettingsViewController: UIViewController, UITableViewDelegate, UITa
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if (section == Sections.People.rawValue) {
             return post.membersHelpingOut.count
+        } else if (section == Sections.Buttons.rawValue) {
+            return 2
         } else {
             return 1
         }
@@ -99,6 +107,27 @@ class RequestSettingsViewController: UIViewController, UITableViewDelegate, UITa
         case Sections.People.rawValue:
             let cell = tableView.dequeueReusableCellWithIdentifier("peopleHelpingCell", forIndexPath: indexPath)
             cell.textLabel?.text = post.membersHelpingOut[indexPath.item].name
+            return cell
+        case Sections.Buttons.rawValue:
+            let cell = tableView.dequeueReusableCellWithIdentifier("buttonCell", forIndexPath: indexPath)
+            cell.backgroundColor = UIColor.appBackgroundColor()
+            if let cell = cell as? ButtonTableViewCell {
+                
+                cell.actionButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+                cell.actionButton.layer.cornerRadius = 5.0
+                cell.actionButton.contentEdgeInsets = UIEdgeInsetsMake(10, 20, 10, 20)
+
+                switch (indexPath.item) {
+                case 0:
+                    cell.actionButton.setTitle("Cancel Request", forState: .Normal)
+                    cell.actionButton.backgroundColor = UIColor.blackColor()
+                case 1:
+                    cell.actionButton.setTitle("Resolve Request", forState: .Normal)
+                    cell.actionButton.backgroundColor = UIColor.appBlue()
+                default:
+                    break
+                }
+            }
             return cell
         default:
             return tableView.dequeueReusableCellWithIdentifier("", forIndexPath: indexPath)
